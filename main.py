9d0f05c5dca2966b23629cc36984c2d56eabbfd9
Jacob Nave <jacobcnave1@gmail.com>
2021-01-19 18:25:17 -0800

    addtask script added

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from createdb import getNextTasks
 from createdb import allTasks
 from createdb import clearTasks
 from createdb import removeTask
-from createdb import  getNextWeek
+from createdb import getNextWeek
 from createdb import removeOldTasks
 from twilio.rest import Client
 
 
 if __name__ == '__main__':
-    accout_file = open('account.txt', 'r')
+    account_file = open('account.txt', 'r')
 
-    account_sid = accout_file.readline()
-    auth_token = accout_file.readline()
+    account_sid = account_file.readline()
+    auth_token = account_file.readline()
 
     tasks = getNextTasks()
-    task_string = 'Tomorrows task:\n'
+    task_string = "Tomorrow's task:\n"
     for i in range(len(tasks)):
         task_string += str(i+1) + '. ' + tasks[i][1] + ': ' + tasks[i][3] + '\n'
 
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-        to=accout_file.readline(),
-        from_= accout_file.readline(),
+        to=account_file.readline(),
+        from_= account_file.readline(),
         body = task_string
     )
 
